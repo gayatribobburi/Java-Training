a3d3d70f02c3c8f74ba477aa9b5a352750a42ba2
Gayatri <bobburi.gayatri@dxc.com>
2021-12-31 00:36:02 -0800

    Java-Training

---
+++ Assignmentt1.java
@@ -0,0 +1,49 @@
+package Assignments;
+import java.io.*; 
+  
+public class Assignmentt1
+{ 
+    public static void main(String[] args) 
+    { 
+  
+        // Declaring variables 
+        int num1 = 10, num2 = 20; 
+        int res;
+        System.out.println("Initial value of num1 = " + num1); 
+        System.out.println("Initial value of num2 = " + num2); 
+        // Adding & Assigning values 
+        num1 += num2; 
+        // Displaying the assigned values 
+        System.out.println("Value of num1 after += is " + num1);
+        // Subtracting & Assigning values 
+        num1 -= num2; 
+        // Displaying the assigned values 
+        System.out.println("Value of num1 after -= is " + num1);
+        // Multiplying & Assigning values 
+        num1 *= num2; 
+        // Displaying the assigned values 
+        System.out.println("Value of num1 after *= is " + num1);
+        // Dividing & Assigning values 
+        num1 /= num2; 
+        // Displaying the assigned values 
+        System.out.println("Value of num1 after /= is " + num1);
+        // Moduling & Assigning values 
+        num1 %= num2; 
+        // Displaying the assigned values 
+        System.out.println("Value of num1 after %= is " + num1); 
+    } 
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
