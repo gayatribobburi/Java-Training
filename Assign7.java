73aa1a20df1063a876d9c3eb7b28796a1cf34653
Gayatri <bobburi.gayatri@dxc.com>
2022-01-09 11:07:16 -0800

    Java Training

---
+++ Assign7.java
@@ -0,0 +1,13 @@
+public class Assign7 {
+     
+     
+        public static void main(String[] args) {
+            for(int i=1; i<=5; i++){
+            System.out.println("//////////////////////");
+            System.out.println("|| Victory is mine! ||");
+            System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+        }   
+}
+
+}
+
